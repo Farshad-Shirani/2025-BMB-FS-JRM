53ee2789c663a4a37174f6eead367804e6bbb46b
Farshad Shirani <86499329+Farshad-Shirani@users.noreply.github.com>
2025-05-01 23:21:32 -0400

    Add files via upload

---
+++ Range Expansion/AuxiliaryFunctions/calculateAdaptationRate.m
@@ -0,0 +1,65 @@
+function [random,  nonRandom] = calculateAdaptationRate(solution, variableLocation, modelParameters, discretizationParamaters )
+%--- This function is the same as construct_F1. It just returns the two components of F1 associated
+%--- with random and non-random gene flow separaterly. F1 is the some of the two components. 
+
+
+Dx = discretizationParamaters.Dx;
+D = modelParameters.D;
+V_u = modelParameters.V_u;
+Q_opt = modelParameters.Q_opt;
+A_max = modelParameters.A_max;
+dQ_tilde = modelParameters.dQ_tilde;
+
+sigma_squared = D;
+
+Ad = (A_max ./ V_u) .* dQ_tilde;
+AQd = Ad .* ( solution(:,2) - Q_opt );
+AVd = Ad .* solution(:,3);
+%--------------------------------------
+solution_minusTwo = reflect(solution,[2 0]); % solution(i-2)  reflect also applies the reflecting boundary condition
+solution_minusOne = reflect(solution,[1 0]); % solution(i-1)  
+solution_plusOne = reflect(solution,[-1 0]); % solution(i+1)  
+solution_plusTwo = reflect(solution,[-2 0]); % solution(i+2)
+
+Ad_minusTwo = reflect(Ad,[2 0]); % Ad(i-2)  reflect also applies the reflecting boundary condition
+Ad_minusOne = reflect(Ad,[1 0]); % Ad(i-1)  
+Ad_plusOne = reflect(Ad,[-1 0]); % Ad(i+1)  
+Ad_plusTwo = reflect(Ad,[-2 0]); % Ad(i+2)
+
+AQd_minusTwo = reflect(AQd,[2 0]); % AQd(i-2)  reflect also applies the reflecting boundary condition
+AQd_minusOne = reflect(AQd,[1 0]); % AQd(i-1)  
+AQd_plusOne = reflect(AQd,[-1 0]); % AQd(i+1)  
+AQd_plusTwo = reflect(AQd,[-2 0]); % AQd(i+2)
+%----------------------------------------
+
+dS = ( -solution_plusTwo + 8 * solution_plusOne - 8 * solution_minusOne + solution_minusTwo ) / (12 * Dx);
+dN_N = dS(:,1) ./ solution(:,1);
+dQ = dS(:,2);
+dV = dS(:,3);
+
+d2S = ( -solution_plusTwo + 16 * solution_plusOne - 30 * solution + 16 * solution_minusOne - solution_minusTwo ) / (12 * Dx^2);
+d2S = d2S'; % reorders 2-dim array d2S so that linear indexing d2S(:) corresponds to current ordeing in U
+
+AVd_minusTwo = Ad_minusTwo .* solution_minusTwo(:,3);
+AVd_minusOne = Ad_minusOne .* solution_minusOne(:,3);
+AVd_plusOne = Ad_plusOne .* solution_plusOne(:,3);
+AVd_plusTwo = Ad_plusTwo .* solution_plusTwo(:,3);
+dAVd = ( -AVd_plusTwo + 8 * AVd_plusOne - 8 * AVd_minusOne + AVd_minusTwo ) / (12 * Dx);
+
+ANQd_minusTwo = AQd_minusTwo .* solution_minusTwo(:,1);
+ANQd_minusOne = AQd_minusOne .* solution_minusOne(:,1);
+ANQd_plusOne = AQd_plusOne .* solution_plusOne(:,1);
+ANQd_plusTwo = AQd_plusTwo .* solution_plusTwo(:,1);
+dANQd = ( -ANQd_plusTwo + 8 * ANQd_plusOne - 8 * ANQd_minusOne + ANQd_minusTwo ) / (12 * Dx);
+
+random = d2S(:);
+random(variableLocation(1,:)) = random(variableLocation(1,:)) * sigma_squared;
+random(variableLocation(2,:)) = ( random(variableLocation(2,:)) + 2 * dN_N .* dQ ) * sigma_squared;
+random(variableLocation(3,:)) = ( random(variableLocation(3,:)) + 2 * dN_N .* dV + 2 * dQ.^2 ) * sigma_squared;
+
+nonRandom = zeros(size(random));
+nonRandom(variableLocation(1,:)) = - dANQd;
+nonRandom(variableLocation(2,:)) = - dAVd - dN_N .* AVd - dQ .* AQd;
+nonRandom(variableLocation(3,:)) = - 2 * dQ .* AVd - dV .* AQd;
+
+end
---
+++ Range Expansion/Main_Simulation.m
@@ -0,0 +1,138 @@
+clear all
+close all
+
+addpath('AuxiliaryFunctions'); % contains all functions used by the main functions and scripts
+
+%% Initialization ===========================================================================================
+[initialPopulation, modelParameters, simulationParameters, discretizationParamaters, epsilon, eta] = initializeSimulation;
+
+storageTimes = simulationParameters.times;
+Dt = discretizationParamaters.Dt;
+Dx = discretizationParamaters.Dx;
+t = 0 : discretizationParamaters.Dt : simulationParameters.T;
+x = simulationParameters.x_0 : Dx : simulationParameters.x_I;
+I = length(x);
+
+numVariables = length( fieldnames(initialPopulation) );   %number of variables 
+
+population = struct('density', [], 'trait_mean', [], 'trait_variance', []);
+
+population.density = zeros(I, length(storageTimes)); 
+population.trait_mean = zeros(I, length(storageTimes)); 
+population.trait_variance = zeros(I, length(storageTimes));
+
+population.density(:,1) = initialPopulation.density; 
+population.trait_mean(:,1) = initialPopulation.trait_mean; 
+population.trait_variance(:,1) = initialPopulation.trait_variance;
+
+%% Two-stage stabilizing correction ADI scheme [Hundsdorfer, App Num Math, 2000, p.222 =======================
+systemSize = numVariables;
+%---finding the location of the elements of solution matrices in U. 
+%---varLocations(i,j) = linear index of the element at j_th row (j_th grid point) and i_th column (i_th variable) in solution matrix 
+variableLocation = zeros(systemSize, I);
+for i = 1:systemSize
+    variableLocation(i,:) = [1 : systemSize : systemSize * I ] + (i-1);
+end
+
+solution = zeros(I,systemSize);
+%---initializing solution equal to the initial values
+solution(:, 1) = population.density(:,1);
+solution(:, 2) = population.trait_mean(:,1);
+solution(:, 3) = population.trait_variance(:,1);
+
+odeSystemSize = numel(solution);
+U = zeros(odeSystemSize,1);
+for i = 1 : systemSize
+    U(variableLocation(i,:)) = solution(:,i);
+end
+
+solution_star = zeros(I, systemSize);
+storageCounter = 1;
+
+figure, fig1 = axes;
+figure, fig2 = axes;
+figure, fig3 = axes;
+
+for k = 1 : length(t)-1
+    
+%     plot(fig1, x, solution(:,1));
+%     plot(fig2, x, solution(:,2));
+%     plot(fig3, x, solution(:,3));
+%      drawnow
+    %tic
+          
+    %---Stage 1---------------------------------------------------------------------------------------------
+    F1 = construct_F1(solution, variableLocation, Dx);
+    [F2, A2] = construct_F2andA2(solution, variableLocation);
+    F = F1 + F2;
+    
+    W0_star = U + Dt * F;
+    
+    A1 = construct_A1(solution, variableLocation, Dx);
+    W1_star = ( speye(odeSystemSize) - eta * Dt * A1 ) \ ( W0_star - eta * Dt * A1 * U );
+    
+    U_star = ( speye(odeSystemSize) - eta * Dt * A2 ) \ ( W1_star - eta * Dt * A2 * U ); % U_star = W2_star
+    
+    for i = 1 : systemSize
+        solution_star(:,i) =  U_star(variableLocation(i,:));
+    end
+      
+    %---Stage 2---------------------------------------------------------------------------------------------
+    F1 = construct_F1(solution_star, variableLocation, Dx);
+    [F2, A2] = construct_F2andA2(solution_star, variableLocation);
+    F_star = F1 + F2;
+    
+    W0 = U + 1/2 * Dt * ( F + F_star );
+    
+    A1 = construct_A1(solution_star, variableLocation, Dx);
+    W1 = ( speye(odeSystemSize) - eta * Dt * A1 ) \ ( W0 - eta * Dt * A1 * U_star );
+  
+    U = ( speye(odeSystemSize) - eta * Dt * A2 ) \ ( W1 - eta * Dt * A2 * U_star ); % U = W2
+    
+    for i = 1 : systemSize
+        solution(:,i) =  U(variableLocation(i,:));
+    end
+          
+    %---solution storage-----------------------------------------------------
+    if storageCounter + 1 <= length(storageTimes) 
+        if abs( t(k+1) - storageTimes(storageCounter + 1) ) <= Dt/2
+            storageCounter = storageCounter + 1;
+            simulationParameters.times(storageCounter) = t(k+1); % actual storage times might be different that what set in simulation initialization (depending on the value of Dt)
+            population.density(:,storageCounter) = solution(:,1);
+            population.trait_mean(:,storageCounter) = solution(:,2);
+            population.trait_variance(:,storageCounter) = solution(:,3);
+        end 
+    end
+   % time = t(k+1) % Display current iteration time to monitor the progress
+    %toc
+
+    
+    if sum(isnan(U)) % Means there is an error or the solution has diverged
+        disp('Error! NaN entries are present in the solution')
+        break
+    end    
+       
+end 
+
+%save('Results\sol_A_max_10.mat', 'population', 'modelParameters', 'simulationParameters', 'discretizationParamaters');
+
+%save('Results\sol_A_max_0_gradient_1_5.mat', 'population', 'modelParameters', 'simulationParameters', 'discretizationParamaters');
+
+%save('Results\sol_A_max_0_V_u_1.mat', 'population', 'modelParameters', 'simulationParameters', 'discretizationParamaters');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
